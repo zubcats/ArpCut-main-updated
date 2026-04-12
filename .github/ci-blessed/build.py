d26ce8c9ac4e4a9ec57688b0a73d15fc596938e9
zubcats <zubcats@gmail.com>
2026-04-12 12:47:16 -0400

    Update build.py

--- .github/ci-blessed/build.py
+++ .github/ci-blessed/build.py
@@ -13,7 +13,7 @@ import platform
 _ROOT = os.path.dirname(os.path.abspath(__file__))
 if _ROOT not in sys.path:
     sys.path.insert(0, os.path.join(_ROOT, 'src'))
-from constants import APP_BUNDLE_NAME
+from src.constants import APP_BUNDLE_NAME
 
 HIDDEN_IMPORTS = [
     'PyQt5',
